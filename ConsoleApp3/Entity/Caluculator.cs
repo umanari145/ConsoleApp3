b96cede9b94cc221451868d23d530737dd56910c
MATSUMOTO-WIN11\matsumoto <umanari145@gmail.com>
2026-07-26 10:46:03 +0900

    MSTESTのプロジェクトを追加

---
+++ ConsoleApp3/Entity/Caluculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3.Entity
+{
+    internal class Caluculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
