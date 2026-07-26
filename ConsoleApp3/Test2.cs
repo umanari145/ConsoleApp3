279c215e097a41df57646c6d234d49f2a0947cec
MATSUMOTO-WIN11\matsumoto <umanari145@gmail.com>
2026-07-26 11:00:25 +0900

    testプロジェクトをコミット

---
+++ ConsoleApp3/Test2.cs
@@ -0,0 +1,15 @@
+using ConsoleApp3.Util;
+
+namespace ConsoleApp3;
+
+[TestClass]
+public class Test2
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        CollectionSample12 cs12 = new CollectionSample12();
+
+        cs12.output();
+    }
+}
